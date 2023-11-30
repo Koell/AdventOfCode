fc3e974e6bdcac435a97c2e929bb5cf0363ae8cb
Fabian Koell <fkoell@koell>
2023-11-30 17:28:01 +0100

    add namespace

--- 2023/AOC_2023/helperFunctions.cs
+++ 2023/AOC_2023/helperFunctions.cs
@@ -1,19 +1,22 @@
 using System.Collections.Generic;
 using System.IO;
 
-public class HelperFunctions {
+namespace AOC_2023 {
+    public class HelperFunctions {
 
-    public static List<string> ExtractList(string fileName) {
-        List<string> lines = new List<string>();
-        try {
-            using (StreamReader reader = new StreamReader(fileName)) {
-                string line;
-                while ((line = reader.ReadLine()) != null) {
-                    lines.Add(line);
+        public static List<string> ExtractList(string fileName) {
+            List<string> lines = new List<string>();
+            try {
+                using (StreamReader reader = new StreamReader(fileName)) {
+                    string line;
+                    while ((line = reader.ReadLine()) != null) {
+                        lines.Add(line);
+                    }
                 }
             }
-        } catch { }
+            catch { }
 
-        return lines;
+            return lines;
+        }
     }
 }
--- 2023/AOC_2023/tmp.cs
+++ 2023/AOC_2023/tmp.cs
@@ -1,53 +1,52 @@
 
     using System.Text.RegularExpressions;
 
-    class Temp {
-        
-        public static void Solver() {
-            string fileName = "tmp";
-            string filePath = System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName;
-            filePath = Regex.Replace(filePath, "bin.*", "");
-            
-            
-            List<string> testInput = HelperFunctions.ExtractList($"{filePath}inputs/tinput_{fileName}");
-            List<string> realInput = HelperFunctions.ExtractList($"{filePath}inputs/input_{fileName}");
-            Console.WriteLine("Testrun:");
-            Solve(testInput);
-            Console.WriteLine("\nSolution:");
-            Solve(realInput);
-        }
+    namespace AOC_2023 {
+        class Temp {
 
-        private static void Solve(List<string> input)
-        {
-            int sol1 = Case1(input);
-            int sol2 = Case2(input);
+            public static void Solver() {
+                string fileName = "tmp";
+                string filePath = System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName;
+                filePath = Regex.Replace(filePath, "bin.*", "");
 
-            Console.WriteLine($"case 1: {sol1}");
-            Console.WriteLine($"case 2: {sol2}");
-        }
 
-        static int Case1(List<string> lines)
-        {
-            DateTime startTime = DateTime.Now;
-            int result = 0;
+                List<string> testInput = HelperFunctions.ExtractList($"{filePath}inputs/tinput_{fileName}");
+                List<string> realInput = HelperFunctions.ExtractList($"{filePath}inputs/input_{fileName}");
+                Console.WriteLine("Testrun:");
+                Solve(testInput);
+                Console.WriteLine("\nSolution:");
+                Solve(realInput);
+            }
 
-            // Your case 1 logic here
+            private static void Solve(List<string> input) {
+                int sol1 = Case1(input);
+                int sol2 = Case2(input);
 
-            TimeSpan elapsedTime = DateTime.Now - startTime;
-            Console.WriteLine($"Time needed: {elapsedTime.TotalSeconds} seconds");
-            return result;
-        }
+                Console.WriteLine($"case 1: {sol1}");
+                Console.WriteLine($"case 2: {sol2}");
+            }
 
-        static int Case2(List<string> lines)
-        {
-            DateTime startTime = DateTime.Now;
-            int result = 0;
+            static int Case1(List<string> lines) {
+                DateTime startTime = DateTime.Now;
+                int result = 0;
 
-            // Your case 2 logic here
+                // Your case 1 logic here
 
-            TimeSpan elapsedTime = DateTime.Now - startTime;
-            Console.WriteLine($"Time needed: {elapsedTime.TotalSeconds} seconds");
-            return result;
+                TimeSpan elapsedTime = DateTime.Now - startTime;
+                Console.WriteLine($"Time needed: {elapsedTime.TotalSeconds} seconds");
+                return result;
+            }
+
+            static int Case2(List<string> lines) {
+                DateTime startTime = DateTime.Now;
+                int result = 0;
+
+                // Your case 2 logic here
+
+                TimeSpan elapsedTime = DateTime.Now - startTime;
+                Console.WriteLine($"Time needed: {elapsedTime.TotalSeconds} seconds");
+                return result;
+            }
         }
     }
 
