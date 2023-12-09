b4a8830e9fa396ae48ab8395615fa1e349b50e86
Fabian Koell <fkoell@koell>
2023-12-09 13:34:57 +0100

    day09 simplify

--- 2023/AOC_2023/day09.cs
+++ 2023/AOC_2023/day09.cs
@@ -33,7 +33,8 @@ namespace AOC_2023 {
                 foreach (Match match in rx_number.Matches(line)) {
                     numbers.Add(long.Parse(match.Value));
                 }
-                result += calculateNextNumberReverse(numbers);
+                numbers.Reverse();
+                result += calculateNextNumber(numbers);
             }
 
             return new Solution(result.ToString(), DateTime.Now - startTime);
@@ -58,26 +59,6 @@ namespace AOC_2023 {
                 numbers[i].Add(numbers[i].Last() + numbers[i+1].Last());
             }
             return numbers.First().Last();
-        }
-        
-        private long calculateNextNumberReverse(List<long> input) {
-            var numbers = new List<List<long>> {
-                input
-            };
-            while (!numbers.Last().TrueForAll(val => val == 0)) {
-                var last = numbers.Last();
-                var new_list = new List<long>();
-                for (int i= 0; i < last.Count -1; i++) {
-                    new_list.Add(last[i+1]-last[i]);
-                }
-                numbers.Add(new_list);
-            }
-            numbers.Last().Insert(0, 0);
-
-            for (int i = numbers.Count - 2; i >= 0; i--) {
-                numbers[i].Insert(0,numbers[i].First() - numbers[i+1].First());
-            }
-            return numbers.First().First();
         }
 
         [GeneratedRegex("(-*\\d+)")]
